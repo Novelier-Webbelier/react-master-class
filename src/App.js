142d914ca9bbe3b49132135c867843b1436ee0a3
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-05 15:26:05 +0900

    2.4 Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
-const Input = styled.input.attrs({
-	required: true
-})`
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+    background-color: red;
+  }
+
+  50%{
+    transform: rotate(180deg);
+    border-radius: 100px;
+    background-color: green;
+  }
+
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+    background-color: blue;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+
+  span {
+    font-size: 30px;
+
+    &:hover {
+      font-size: 10px;
+    }
+
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
 	return (
-		<Father as="header">
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-		</Father>
+		<Wrapper>
+			<Box>
+				<span>🤩</span>
+			</Box>
+		</Wrapper>
 	);
 }
 
