6360314ed7f781f52bbff104a4e41518c5c12811
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-06 16:50:52 +0900

    3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(`Hello ${username}`)
-  }
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={username}
-          onChange={onChange}
-          type="text"
-          placeholder="Username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
-  );
+  return <div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
+  </div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { lightTheme, darkTheme } from "./theme";
 
 const htmlRoot = document.getElementById("root")!;
 const root = ReactDOM.createRoot(htmlRoot);
 
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={lightTheme}>
+    <App />
+  </ThemeProvider>
+);
