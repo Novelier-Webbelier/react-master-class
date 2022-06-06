63a2c8de722a350717d045f905307e2e0ddddad9
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-06 14:06:40 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`Hello ${username}`)
+  }
+
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="tomato" />
-      <Circle text="Hello World" bgColor="teal" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={username}
+          onChange={onChange}
+          type="text"
+          placeholder="Username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
